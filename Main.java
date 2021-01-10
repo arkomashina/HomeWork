d14414ccb89d1fe693c61bacddc011d9dacd0034
arkomashina <77117555+arkomashina@users.noreply.github.com>
2021-01-11 01:08:19 +0300

    add lesson 2

--- Main.java
+++ Main.java
@@ -1,88 +1,64 @@
-package ru.geekbrains.HomeWork_1;
-
+package ru.geekbrains.HomeWork_2;
 public class Main {
-
     public static void main(String[] args) {
-        // Написать метод вычисляющий выражение a * (b + (c / d))
-        // и возвращающий результат,где a, b, c, d – входные параметры этого метода
-        float a = 12;
-        float b = 31;
-        float c = 18.5f;
-        float d = 6;
-        float result = calculation (a, b, c, d);
-        System.out.println("Результат вычислений " + result);
-
-        //Написать метод, принимающий на вход два числа,
-        // и проверяющий что их сумма лежит в пределах от 10 до 20(включительно),
-        // если да – вернуть true, в противном случае – false;
-        int q = 13;
-        int w = 8;
-        System.out.println(isNumberBetween10And20(q, w));
-
-        //Написать метод, которому в качестве параметра передается целое число,
-        // метод должен напечатать в консоль положительное ли число передали,
-        // или отрицательное; Замечание: ноль считаем положительным числом.
-        int number = 14;
-        System.out.println("Является ли число " + number + " Положительным или отрицательным?");
-        System.out.println(isNumberPositiveOrNot(number));
-
-        //Написать метод, которому в качестве параметра передается целое число,
-        // метод должен вернуть true, если число отрицательное;
-        int y = -15;
-        System.out.println("Является ли число " + y + " отрицательным?");
-        System.out.println(isYNegative(y));
-
-        //Написать метод, которому в качестве параметра передается строка,
-        // обозначающая имя, метод должен вывести в консоль сообщение "Привет, указанное_имя!"
-        String name = "Аркадий";
-        System.out.println(printName("Аркадий"));
-
-        //Написать метод, который определяет является ли год високосным,
-        // и выводит сообщение в консоль.
-        // Каждый 4-й год является високосным, кроме каждого 100-го, при этом каждый 400-й – високосный.
-        int year = 2021;
-        System.out.println(ifYearLeapOrNot(year));
-
-
-
-    }
-    public static float calculation(float a, float b, float c, float d){
-        return  a * (b + (c / d));
-    }
-    public static boolean isNumberBetween10And20(int q, int w){
-        int sum = q + w;
-        boolean end = sum >= 10 && sum <= 20;
-        return end;
-    }
-    public static int isNumberPositiveOrNot(int number){
-        if  (number >= 0){
-            System.out.println("Число положительное");
-        } else{
-            System.out.println("Число отрицательное");
+        //Задать целочисленный массив, состоящий из элементов 0 и 1.
+        // Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
+        // С помощью цикла и условия заменить 0 на 1, 1 на 0;
+        int[] array = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        int i;
+        for (i = 0; array.length > i; i++) {
+            if (array[i] == 1) {
+                System.out.print("0");
+            } else System.out.print("1");
         }
-        return number;
+        fillsArray();
+        changeArray();
+        diagonalArray();
     }
-    public static int isYNegative(int y){
-        if (y < 0){
-            System.out.println("True");
-        } else {
-            System.out.println("False");
+    public static void fillsArray(){
+       //Задать пустой целочисленный массив размером 8.
+       //С помощью цикла заполнить его значениями 0 3 6 9 12 15 18 21
+        int[] array = new int[8];
+        int i;
+        for (i = 0; i <= 21; i = i + 3){
+            if(i == 0);{
+                System.out.println(i);
+            }
+
         }
-        return y;
     }
-    public static String printName(String name){
-        System.out.println("Привет, " + name);
-        return name;
-    }
-    public static int ifYearLeapOrNot(int year){
-        if (year % 4 == 0 && year % 100 != 0){
-            System.out.println(year + " год является високосным");
-        } else if (year % 400 == 0){
-            System.out.println(year + " год является високосным");
-        } else {
-            System.out.println(year + " год не является високосным");
+    public static void changeArray(){
+        //Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ]
+        // пройти по нему циклом, и числа меньшие 6 умножить на 2
+        int[] array = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        int i;
+        int j = 1;
+        for (i = array[0]; i < 12; i = array[j++]){
+            if(i < 6) {
+                System.out.println(i * 2);
+            }else{
+                System.out.println(i);
+            }
         }
-        return year;
     }
+    public static void diagonalArray(){
+        //Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое),
+        // и с помощью цикла(-ов) заполнить его диагональные элементы единицами
+        int[][] SquareArray = new int[5][5];
+        for(int i = 0;i < SquareArray.length; i++)
+        {
+            for(int j = 0;j < SquareArray.length; j++)
+            {
+                if(i == j)
+                {
+                    SquareArray[i][j] = 1;
+                }
+                System.out.print(SquareArray[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+
 
 }
